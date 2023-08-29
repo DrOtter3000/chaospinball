39e9e3decce791b65203bc4eec15314a4ff2d710
drotter3000 <xtristrix@cyberiade.de>
2023-08-29 17:23:23 +0200

    add flippers

---
+++ LeftFlipper.gd
@@ -0,0 +1,16 @@
+extends AnimatableBody3D
+
+
+@export var target_angle = 45
+var normal_position = -154.4
+
+
+func _ready():
+	normal_position = rotation.y
+
+
+func _physics_process(delta):
+	if Input.is_action_pressed("left"):
+		rotation.y += 0.25
+	else:
+		rotation.y = -154.4
---
+++ right_flipper.gd
@@ -0,0 +1,16 @@
+extends AnimatableBody3D
+
+
+@export var target_angle = 45
+var normal_position = 25.6
+
+
+func _ready():
+	normal_position = rotation.y
+
+
+func _physics_process(delta):
+	if Input.is_action_pressed("right"):
+		rotation.y -= 0.25
+	else:
+		rotation.y = 25.6
