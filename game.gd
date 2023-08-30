16202a07d87a1d495c67d061dc3d38e03a789523
drotter3000 <xtristrix@cyberiade.de>
2023-08-30 18:50:06 +0200

    add game_over function

---
+++ game.gd
@@ -0,0 +1,5 @@
+extends Node3D
+
+
+func game_over():
+	pass
--- machine.gd
+++ machine.gd
@@ -4,3 +4,8 @@ extends Node3D
 func _process(delta):
 	if Input.is_action_just_pressed("fire"):
 		$AnimationPlayer.play("fire")
+
+
+func _on_game_over_area_body_entered(body):
+	if body.is_in_group("Ball"):
+		get_tree().call_group("Game", "game_over")
