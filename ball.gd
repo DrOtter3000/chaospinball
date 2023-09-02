8170655222c753a896b3c6213058d24ed904c35d
drotter3000 <xtristrix@cyberiade.de>
2023-09-02 19:13:03 +0200

    change movement of TriangleOfFun

--- ball.gd
+++ ball.gd
@@ -2,12 +2,4 @@ extends RigidBody3D
 
 
 func _ready():
-	randomize()
-
-
-func _on_timer_timeout():
-	var material = randi_range(0,2)
-	if material == 0:
-		physics_material_override.resource_path = "res://Materials/BallPhysicsMaterial.tres"
-	elif material == 1:
-		physics_material_override.resource_path = "res://Materials/BallPhysicsMaterial_fullBounce.tres"
+	pass
--- machine.gd
+++ machine.gd
@@ -6,7 +6,7 @@ extends Node3D
 
 func _process(delta):
 	if Input.is_action_just_pressed("fire"):
-		$AnimationPlayer.play("fire")
+		$SpringPlayer.play("fire")
 
 
 func _on_game_over_area_body_entered(body):
@@ -29,14 +29,3 @@ func teleport_particles(pos):
 	particles.position = pos
 
 
-func _on_timer_timeout():
-	var change_status = randi_range(0,2)
-	print(change_status)
-	if change_status == 1:
-		$TriangleOfFun.scale = Vector3(1,1,1)
-		$TriangleOfFun.rotate_y(randi_range(-180, 180))
-	if change_status == 2:
-		$TriangleOfFun.scale = Vector3(1,1,1)
-		$TriangleOfFun.position.x = randi_range(-15, 15)
-	else:
-		$TriangleOfFun.scale = Vector3.ZERO
