078b01e7a1c239bf80ef706a64ad2013dfbd2283
drotter3000 <xtristrix@cyberiade.de>
2023-09-04 18:59:54 +0200

    add warpsounds

--- machine.gd
+++ machine.gd
@@ -2,6 +2,7 @@ extends Node3D
 
 
 @export var newParticles :PackedScene
+var warp_array = ["res://SFX/Warp_Sounds/Warp_1.mp3", "res://SFX/Warp_Sounds/Warp_2.mp3", "res://SFX/Warp_Sounds/Warp_3.mp3", "res://SFX/Warp_Sounds/Warp_4.mp3", "res://SFX/Warp_Sounds/Warp_5.mp3", "res://SFX/Warp_Sounds/Warp_6.mp3"]
 
 
 func _process(delta):
@@ -17,11 +18,14 @@ func _on_ball_lost_area_body_entered(body):
 
 
 func _on_area_3d_body_entered(body):
+	var random_warp = warp_array[randi_range(0, warp_array.size() -1)]
 	if body.is_in_group("Ball"):
 		var exit_list = get_node("PortalExits").get_children()
 		var random_exit = exit_list[randi_range(0, exit_list.size()-1)]
 		body.position = random_exit.global_position
 		teleport_particles(random_exit.position)
+		$AudioStreamPlayer.stream = load(random_warp)
+		$AudioStreamPlayer.play()
 
 
 func teleport_particles(pos):
