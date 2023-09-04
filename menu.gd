5905b6b0d4170392000ba49f8f5cdab5d76918e5
drotter3000 <xtristrix@cyberiade.de>
2023-09-04 20:25:46 +0200

    add main menu messages

--- menu.gd
+++ menu.gd
@@ -1,6 +1,15 @@
 extends Control
 
 
+var quote_array = ["Chaos can be boring too", "Boring can be annoying too", "All sounds DIY... next time I will use bought ones again", "Upsi Woopsi, a wittle fuckoboingo", "Why? Just WHY?"]
+
+
+func _ready():
+	randomize()
+	var random_text = quote_array[randi_range(0, quote_array.size() -1)]
+	$LblSubtitle.text = random_text
+
+
 func _on_btn_start_pressed():
 	Gamemanager.balls_ready = 10
 	Gamemanager.points = 0
